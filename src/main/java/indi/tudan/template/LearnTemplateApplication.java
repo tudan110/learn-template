50e786042c2581d1819dd8ca26476ba5721ccd02
tudan110 <wtudan@126.com>
2019-10-31 21:27:07 +0800

    --

--- src/main/java/indi/tudan/template/LearnTemplateApplication.java
+++ src/main/java/indi/tudan/template/LearnTemplateApplication.java
@@ -1,5 +1,7 @@
 package indi.tudan.template;
 
+import indi.tudan.template.service.GenerateEventDataFile;
+import indi.tudan.template.utils.ClassUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,16 @@ public class LearnTemplateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearnTemplateApplication.class, args);
+
+        //String outPath = System.getProperty("user.dir");
+        String outPath = ClassUtils.getCurrentProgramPath(LearnTemplateApplication.class);
+        String excelPath = outPath + "/测试文件.xlsx";
+        GenerateEventDataFile.builder()
+                .excelPath(excelPath)
+                .sheetIndex(0)
+                .outPath(outPath)
+                .build()
+                .parse();
     }
 
 }
---
+++ src/main/java/indi/tudan/template/utils/ClassUtils.java
@@ -0,0 +1,80 @@
+package indi.tudan.template.utils;
+
+import org.springframework.boot.system.ApplicationHome;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.security.CodeSource;
+import java.util.Objects;
+
+/**
+ * 类的工具类
+ *
+ * @author wangtan
+ * @date 2019-09-28 11:07:12
+ * @since 1.0
+ */
+public class ClassUtils {
+
+    /**
+     * Don't let anyone else instantiate this class
+     */
+    private ClassUtils() {
+    }
+
+    /**
+     * 获取当前程序所在路径
+     *
+     * @return String
+     * @date 2019-09-28 11:08:39
+     * @since 1.0
+     */
+    public static String getCurrentProgramPath() {
+        CodeSource codeSource = ClassUtils.class.getProtectionDomain().getCodeSource();
+
+        File jarFile = null;
+        try {
+            jarFile = new File(codeSource.getLocation().toURI().getPath());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        return Objects.requireNonNull(jarFile, "jar file should not be null.").getParentFile().getPath();
+    }
+
+    /**
+     * 根据类，获取当前程序所在路径
+     *
+     * @param sourceClass 类的类型
+     * @return 所在路径
+     * @date 2019-10-31 14:28:11
+     */
+    public static String getCurrentProgramPath(Class<?> sourceClass) {
+        ApplicationHome h = new ApplicationHome(sourceClass);
+        File jarF = h.getSource();
+        return jarF.getParentFile().toString();
+    }
+
+    /**
+     * 判断由提供的类名（类的全限定名）标识的类是否存在并可以加载
+     * 如果类或其中一个依赖关系不存在或无法加载，则返回false
+     *
+     * @param className 要检查的类的名称
+     * @return 指定的类是否存在
+     * @author wangtan
+     * @date 2019-09-05 09:43:31
+     * @since 1.0
+     */
+    public static boolean isPresent(String className) {
+        try {
+            Thread.currentThread().getContextClassLoader().loadClass(className);
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPresent("indi.tudan.uvnccall.utils.ClassUtils"));
+    }
+}
